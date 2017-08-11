c683d90d4f7f2809f51abbc514e280b1164957e7
heyhey <fruitsensatie@live.nl>
2017-08-11 19:17:15 +0200

    Bezig met fadende tekst

---
+++ js/scrolling.js
@@ -0,0 +1,51 @@
+$("document").ready(function() {			
+				
+				var parallax1 = $(".parallax").get(0);
+
+				$('.first').click(function(){
+										   
+					$('html, body').animate({
+						scrollTop: $(".parallax#parallax1").offset().top
+					}, 2000);				   
+					 
+										   
+				 });
+				
+				
+				
+				$('.second').click(function(){
+										   
+					$('html, body').animate({
+						scrollTop: $(".parallax#parallax2").offset().top
+					}, 2000);				   
+					 
+										   
+				 });
+				
+				
+					$('.third').click(function(){
+										   
+					$('html, body').animate({
+						scrollTop: $(".parallax#parallax3").offset().top
+					}, 2000);				   
+					 
+										   
+				 });
+
+});
+
+/*window.onscroll = function() {myFunction()};
+
+function myFunction() {
+    if (document.body.scrollTop > 800 || document.documentElement.scrollTop > 800) {
+        document.getElementById("test").className = "titleContainer";
+    } 
+}*/
+
+$(window).scroll(function() {
+  if ($(this).scrollTop()> 100) {
+    $('.scrolltop').fadeIn();
+   } else {
+    $('.scrolltop').fadeOut();
+   }
+});
